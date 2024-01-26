bd771b114aec967933287e87c9ebd2f3d07ded4c
Isaque Amorim <isaqueamorim25@gmail.com>
2024-01-26 16:14:43 -0300

    :recycle: Reorganizando infra do projeto

--- web/src/infrasctructure/checkout/repository/sequelize/order.repository.spec.ts
+++ web/src/infrasctructure/checkout/repository/sequelize/order.repository.spec.ts
@@ -1,15 +1,15 @@
 import { Sequelize } from "sequelize-typescript";
-import Address from "../../domain/customer/value-objects/address";
-import Customer from "../../domain/customer/entity/customer";
-import Order from "../../domain/checkout/entity/order";
-import OrderItem from "../../domain/checkout/entity/order_item";
-import Product from "../../domain/product/entity/product";
-import CustomerRepository from "./customer.repository";
-import ProductRepository from "./product.repository";
-import CustomerModel from "../db/sequelize/model/customer.model";
-import ProductModel from "../db/sequelize/model/product.model";
-import OrderItemModel from "../db/sequelize/model/order-item.model";
-import OrderModel from "../db/sequelize/model/order.model";
+import Address from "../../../../domain/customer/value-objects/address";
+import Customer from "../../../../domain/customer/entity/customer";
+import Order from "../../../../domain/checkout/entity/order";
+import OrderItem from "../../../../domain/checkout/entity/order_item";
+import Product from "../../../../domain/product/entity/product";
+import CustomerRepository from "../../../customer/repository/sequelize/customer.repository";
+import ProductRepository from "../../../product/repository/sequelize/product.repository";
+import CustomerModel from "../../../customer/repository/sequelize/customer.model";
+import ProductModel from "../../../product/repository/sequelize/product.model";
+import OrderItemModel from "./order-item.model";
+import OrderModel from "./order.model";
 import OrderRepository from "./order.repository";
 
 describe("Order repository test", () => {
